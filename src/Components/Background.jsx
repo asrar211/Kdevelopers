4ff7038f71107df11c53d8e36634df7920bca34e
Asrar <aliasrar84@gmail.com>
2024-03-08 19:43:46 +0530

    helo

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const Background = () => {
   return (
-    <div className='blur-3xl '>
-      <div className='bgr absolute w-[90vw] h-20'></div>
-      <div className='bgr1 absolute w-[90vw] h-20'></div>
-      <div className='bgr2 absolute w-[90vw] h-20'></div>
-      </div>
+    <></>
   )
 }
 
--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <>
     <div className='max-[768px]:hidden'>
-     <div className='flex justify-around items-center rounded-full my-4 text-white bg-[rgba(0,0,0,0.3)] sm:w-[50%] ml-[25%] py-2'>
+     <div className='flex justify-around items-center rounded-full my-4 text-white bg-[rgba(0,0,0,0.3)] sm:w-[50%] ml-[42%] py-2'>
       <Link to="/"><h2 className='text-[20px] text-white drop-shadow-2xl px-3 cursor-pointer py-1 hover:text-blue-900 ease-in duration-300'>Kdevs</h2></Link>
       <h4 className='text-[15px] hover:bg-white hover:text-black ease-in duration-500 px-3 cursor-pointer py-1 rounded-full'>Works</h4>
       <h4 className='text-[15px] hover:bg-white hover:text-black ease-in duration-500 px-3 cursor-pointer py-1 rounded-full'>Services</h4>
@@ -26,12 +26,12 @@ const Navbar = () => {
     {/* //hamburger menu */}
     <div onClick={handleClick}>
         {isMenuOpen ? (
-        <div className='min-[768px]:hidden flex flex-col justify-center items-center my-4 bg-[rgba(0,0,0,0.3)] w-[20%] ml-[40%] py-2 rounded-full'>
+        <div className='min-[768px]:hidden flex flex-col justify-center items-center my-4 bg-[rgba(0,0,0,0.3)] w-[20%] ml-[55%] py-2 rounded-full'>
         <div className='w-[20px] rotate-45 translate-y-[6px] ease-in duration-300 bg-white h-[3px] mb-[3px]'> </div>
         <div className='w-[20px] rotate-[-45deg] ease-in duration-300  bg-white h-[3px] mb-[3px]'> </div>
         </div>
         ) : (
-          <div className='min-[768px]:hidden flex flex-col justify-center items-center my-4 bg-[rgba(0,0,0,0.3)] w-[20%] ml-[40%] py-2 rounded-full'>
+          <div className='min-[768px]:hidden flex flex-col justify-center items-center my-4 bg-[rgba(0,0,0,0.3)] w-[20%] ml-[55%] py-2 rounded-full'>
           <div className='w-[20px] ease-in duration-300 bg-white h-[3px]  mb-[3px]'> </div>
           <div className='w-[20px] ease-in duration-300 bg-white h-[3px] mb-[3px]'> </div>
           <div className='w-[20px] ease-in duration-300 bg-white h-[3px] mb-[3px]'> </div>
@@ -42,7 +42,7 @@ const Navbar = () => {
    {/* for mobile screens */}
          {isOpen && (
         <div>
-        <div className='min-[768px]:hidden'>
+        <div className='min-[768px]:hidden w-[100vw]'>
      <div className='flex flex-col justify-around items-center gap-4 my-4 text-white bg-[rgba(0,0,0,0.3)] py-2'>
       <Link to="/"><h2 className='text-[25px] underline text-white drop-shadow-2xl px-3 cursor-pointer py-1 '>Kdevs</h2></Link>
       <h4 className='text-[15px] hover:bg-white hover:text-black ease-in duration-500 px-3 cursor-pointer py-1 rounded-full'>Works</h4>
