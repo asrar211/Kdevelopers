a5f5f4b59645a35c76e0b2f720c4366944698280
Asrar <aliasrar84@gmail.com>
2024-03-09 14:18:14 +0530

    next

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -6,8 +6,6 @@ const Background = () => {
     <div>
         <div className='w-[400px] h-[400px] blur-[15rem] absolute translate-x-[-50%] translate-y-[-50%] bg-blue-300 z-[-1] '></div>
         <div>
-            <video src={v1} loop autoPlay className='absolute min-[900px]:hidden z-[-1] blur-sm'></video>
-            <video src={v1} loop autoPlay className='absolute top-[30.4rem] min-[900px]:hidden z-[-1] blur-sm '></video>
         </div>
     </div>
   )
