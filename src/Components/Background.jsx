654e634b1cd1c2d4faea424c0289c70cbf3dc41a
Asrar <aliasrar84@gmail.com>
2024-03-09 14:12:46 +0530

    next

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import v1 from "../../public/assets/v3.mp4"
+import v1 from "../../public/assets/v1.mp4"
 
 const Background = () => {
   return (
     <div>
         <div className='w-[400px] h-[400px] blur-[15rem] absolute translate-x-[-50%] translate-y-[-50%] bg-blue-300 z-[-1] '></div>
         <div>
-            <video src={v1} loop autoPlay className='absolute min-[900px]:hidden z-[-1] blur-md'></video>
+            <video src={v1} loop autoPlay className='absolute min-[900px]:hidden z-[-1] blur-sm'></video>
+            <video src={v1} loop autoPlay className='absolute top-[30.4rem] min-[900px]:hidden z-[-1] blur-sm '></video>
         </div>
     </div>
   )
