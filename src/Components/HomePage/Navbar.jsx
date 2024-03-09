81aad034a9c6c0aa4d3c959e8474b87f2658adcb
Asrar <aliasrar84@gmail.com>
2024-03-09 19:28:13 +0530

    nxt

--- src/Components/HomePage/Navbar.jsx
+++ src/Components/HomePage/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CiDark, CiLight } from "react-icons/ci";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -32,7 +33,7 @@ const Navbar = () => {
     <div className='flex justify-between items-center my-4 mx-5 min-[900px]:hidden'>
       <h2 className='text-xl text-black '>Kdevs.</h2>
       <div className='flex gap-5 items-center justify-center'>
-        <div className='text-black'>Icon</div>
+        <div className='text-black'><CiDark size={25}/></div>
         {!isMenuOpen ? (
           <div onClick={handleClick} className='flex flex-col gap-1 items-end '>
             <div className='w-[20px] ease-in duration-300 h-[3px] bg-black/90'></div>
@@ -48,13 +49,13 @@ const Navbar = () => {
       </div>
     </div>
 
-     <div className={`min-[900px]:hidden ${isOpen ? 'bg-black/80' : 'hidden'}`}>
-      <div className='flex flex-col gap-3 py-5 items-center text-white '>
+     <div className={`min-[900px]:hidden ${isOpen ? 'bg-[rgba(0,0,0,0.1)]' : 'hidden'}`}>
+      <div className='flex flex-col gap-3 py-5 items-center text-black/70 '>
         <h4 className='ease-in duration-500 hover:underline cursor-pointer'>Works</h4>
         <h4 className='ease-in duration-500 hover:underline cursor-pointer'>Services</h4>
         <h4 className='ease-in duration-500 hover:underline cursor-pointer'>Support</h4>
       </div>
-      <div className='flex flex-col items-center text-white py-2'>
+      <div className='flex flex-col items-center text-black/70 py-2'>
         <h4>SignIn</h4>
       </div>
      </div>
