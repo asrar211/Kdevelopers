2a8e186675ea34cdc8476901afc38bba170022f7
Asrar <aliasrar84@gmail.com>
2024-03-12 12:52:11 +0530

    nxt

--- src/Components/HomePage/Hero.jsx
+++ src/Components/HomePage/Hero.jsx
@@ -14,8 +14,8 @@ const Hero = () => {
       <h1 className='max-[900px]:text-[1.8rem] min-[900px]:text-[4rem] font-semibold min-[900px]:mt-16 my-5 max-[900px]:text-black text-white'>We Make Everything Look <br /> As Good As You Want</h1>
       <p className='text-[13px] min-[900px]:text-l min-[900px]:text-white/90
       min-[900px]:mx-[20rem] mx-16 opacity-90 text-black/90 my-3'>Our Products seek to immersive extended reality experience that inspire  and westruck first time and experienced user alike.</p>
-      <button className='mt-10 max-[900px]:bg-black/70 max-[900px]:text-white bg-white/70 p-2 mx-2 text-sm font-semibold '>Contact Us -> </button>
-      <button className='bg-black/40 p-2 text-sm mx-2 text-white font-semibold '>Learn More</button>
+      <button className='mt-10 max-[900px]:bg-black/80 rounded-lg max-[900px]:text-white bg-white/70 p-2 mx-2 text-sm font-semibold '>Contact Us -> </button>
+      {/* <button className='bg-black/40 p-2 text-sm mx-2 text-white font-semibold '>Learn More</button> */}
     </div>
       <img src={img1} alt="" 
       className='mt-5 min-[900px]:w-[50%] min-[900px]:ml-[25%]'
