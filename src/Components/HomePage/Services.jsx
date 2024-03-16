e57012066c7831ef5307bb1ac6a33e4cc5baf091
Asrar <aliasrar84@gmail.com>
2024-03-16 13:11:29 +0530

    nxt

--- src/Components/HomePage/Services.jsx
+++ src/Components/HomePage/Services.jsx
@@ -6,25 +6,25 @@ import { GrCloudSoftware } from "react-icons/gr";
 const Services = () => {
   return (
     <div className='mt-10'>
-        <div className='w-[50%] h-[100vh] absolute bg-yellow-300 translate-x-[50%] translate-y-[20%] blur-[4rem] z-[-1]'></div>
+        <div className='min-[900px]:hidden w-[50%] h-[100vh] absolute bg-yellow-300 translate-x-[50%] translate-y-[20%] blur-[4rem] z-[-1]'></div>
         <div className='text-center'>
 
-            <h2 className='text-[1.6rem] font-bold '>Services we Provide</h2>
-            <p className='mx-20 mt-5 text-sm '>We are redefining the way the world communicated. Works and Works. </p>
+            <h2 className='min-[900px]:text-[3rem] min-[900px]:text-white/90 text-[1.6rem] font-bold '>Services we Provide</h2>
+            <p className='min-[900px]:text-lg min-[900px]:text-white/90 mx-20 mt-5 text-sm '>We are redefining the way the world communicated. Works and Works. </p>
         </div>
 
-        <div className='mt-10 flex flex-col items-center'>
-            <div className=' w-[60%] py-10 ease-in duration-300 hover:bg-[rgba(133,131,131,0.1)]'>
+        <div className='min-[900px]:flex-row min-[900px]:mx-[10rem] mt-10 flex flex-col items-center'>
+            <div className=' w-[60%] min-[900px]:w-[50%] min-[900px]:text-white py-10 ease-in duration-300 min-[900px]:hover:bg-black/40 hover:bg-[rgba(133,131,131,0.1)]'>
                 <div className='ml-5 pl-1 rounded-full bg-[rgba(0,0,0,0.2)] w-[10%]'><CiPen size={25}/></div>
                 <h2 className='ml-5 font-semibold'>UI/UX Design</h2>
                 <p className='mx-5 text-l opacity-80'>High quality and cutting edge design for software products.</p>
             </div>
-            <div className=' w-[60%] py-10 hover:bg-[rgba(133,131,131,0.1)] ease-in duration-300'>
+            <div className=' w-[60%] min-[900px]:w-[50%] min-[900px]:text-white  py-10 min-[900px]:hover:bg-black/40 hover:bg-[rgba(133,131,131,0.1)] ease-in duration-300'>
             <div className='ml-5 pl-1 rounded-full bg-[rgba(0,0,0,0.2)] w-[10%]'><CgWebsite size={23}/></div>
                 <h2 className='ml-5 font-semibold'>Web Development</h2>
                 <p className='mx-5 text-l opacity-80'>With a design first methodology we are kept providing user friendly websites.</p>
             </div>
-            <div className=' w-[60%] py-10 hover:bg-[rgba(133,131,131,0.1)] ease-in duration-300'>
+            <div className=' w-[60%] min-[900px]:w-[50%] min-[900px]:text-white  py-10 min-[900px]:hover:bg-black/40 hover:bg-[rgba(133,131,131,0.1)] ease-in duration-300'>
             <div className='ml-5 pl-1 rounded-full bg-[rgba(0,0,0,0.2)] w-[10%]'><GrCloudSoftware size={23}/></div>
                 <h2 className='ml-5 font-semibold'>Software Development</h2>
                 <p className='mx-5 text-l opacity-80'>With a design first methodology we are kept providing user friendly websites.</p>
--- src/Components/HomePage/Works.jsx
+++ src/Components/HomePage/Works.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import img from "../../assets/img1.png"
 
 const Works = () => {
   return (
-   <div>Works</div>
+    <>
+   <div className='mt-10'>
+    <div className='w-[50%] h-[50rem] bg-red-500 absolute translate-x-[50%] z-[-1] blur-[3rem]'>.</div>
+        <h2 className='text-center text-[1.5rem] font-bold'>Works</h2>
+    <div className='mt-5 bg'>
+    <div className='flex flex-col items-center  mx-20 bg-[rgba(0,0,0,0.1)]'>
+        <img src={img} alt="" 
+        className='w-[80%]'/>
+        <h4 className='text-lg font-semibold'>Title</h4>
+        <p className='text-sm opacity-80 text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, doloribus?</p>
+        <button className='bg-black/60 text-white px-1 my-5 rounded-sm font-semibold text-l'>Visit</button>
+    </div>
+   </div>
+
+   <div className='mt-5'>
+    <div className='flex flex-col items-center  mx-20 bg-[rgba(0,0,0,0.1)]'>
+        <img src={img} alt="" 
+        className='w-[80%] '/>
+        <h4 className='text-lg font-semibold'>Title</h4>
+        <p className='text-sm opacity-80 text-center'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi, doloribus?</p>
+        <button className='bg-black/60 text-white px-1 my-5 rounded-sm font-semibold text-l'>Visit</button>
+    </div>
+   </div>
+   </div>
+   </>
   )
 }
 
