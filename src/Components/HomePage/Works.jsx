ccdfd2d1fb9b3e58423a14b1d1478406d246b9a4
Asrar <aliasrar84@gmail.com>
2024-03-22 13:15:09 +0530

    nxt

--- src/Components/HomePage/Works.jsx
+++ src/Components/HomePage/Works.jsx
@@ -8,7 +8,7 @@ const Works = () => {
     <div className='w-[50%] h-[50rem] bg-red-500 absolute translate-x-[50%] z-[-1] blur-[3rem]'>.</div>
         <h2 className='text-center text-[1.5rem] font-bold'>Works</h2>
     <div className='mt-5 bg'>
-    <div className='flex flex-col items-center  mx-20 bg-[rgba(0,0,0,0.1)]'>
+    <div className='flex flex-col items-center  mx-10 bg-[rgba(0,0,0,0.1)]'>
         <img src={img} alt="" 
         className='w-[80%]'/>
         <h4 className='text-lg font-semibold'>Title</h4>
@@ -18,7 +18,7 @@ const Works = () => {
    </div>
 
    <div className='mt-5'>
-    <div className='flex flex-col items-center  mx-20 bg-[rgba(0,0,0,0.1)]'>
+    <div className='flex flex-col items-center  mx-10 bg-[rgba(0,0,0,0.1)]'>
         <img src={img} alt="" 
         className='w-[80%] '/>
         <h4 className='text-lg font-semibold'>Title</h4>
